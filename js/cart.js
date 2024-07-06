16518fb12d1c4432377b916dc22553c714f968fd
monakassem99 <mona.kassem9999@gmail.com>
2024-07-06 13:31:34 +0300

    audiophile project

---
+++ js/cart.js
@@ -0,0 +1,26 @@
+
+// // empty cart drop down show
+function emptyFunc() {
+    document.getElementById("empty-drop").classList.toggle("show");
+}
+
+
+
+// adding items 
+let itemsNo = document.querySelector(".items-no");
+
+let plusCounter = document.querySelector(".plus");
+
+plusCounter.addEventListener('click', function () {
+    itemsNo.innerHTML = Number(itemsNo.innerHTML) + 1;
+})
+
+
+// removing items 
+let minusCounter = document.querySelector(".minus");
+
+minusCounter.addEventListener('click', function () {
+    if (itemsNo.innerHTML > 0) {
+        itemsNo.innerHTML = Number(itemsNo.innerHTML) - 1;
+    }
+})
---
+++ js/main.js
@@ -0,0 +1,41 @@
+// start headphones 
+// see product btn xx99 mark II headphone
+let mark2Btn = document.querySelector("#mark-2-btn")
+function markII() {
+    window.location.href = "xx99-mark-2.html";
+}
+// see product btn xx99 mark I headphone
+let mark1Btn = document.querySelector("#mark-1-btn");
+
+function markI() {
+    window.location.href = "xx99-mark-1.html"
+}
+// see product btn xx59 headphone
+
+function xx59() {
+    window.location.href = "xx59.html"
+}
+
+
+// start speakers 
+// see product btn zx-9-speaker 
+function zx9Speaker() {
+    window.location.href = "zx-9-speaker.html"
+}
+
+// see product btn zx-7-speaker 
+function zx7Speaker() {
+    window.location.href = "zx-7-speaker.html"
+}
+
+//see product yx-1-earphones
+let yx1Earphone = document.querySelector("#yx-1-earphones");
+function yx1EarphoneBtn() {
+    window.location.href = "yx-1-earphones.html";
+}
+
+// go home btn 
+let goBackHome = document.querySelector(".back");
+function goBack() {
+    window.history.back();
+}
